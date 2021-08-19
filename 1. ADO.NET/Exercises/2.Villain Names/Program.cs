955afa1158269030d369f296931b5e7cbedae47c
donchodonev <dummyacc9292@gmail.com>
2021-08-19 15:07:24 +0300

    restructured main folder added another exercise solution

---
+++ 1. ADO.NET/Exercises/2.Villain Names/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _2.Villain_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = @"Server=.;Database=MinionsDB;Integrated Security=true";
+
+            var dbCon = new SqlConnection(target);
+
+            string query = @"
+                            SELECT V.Name, COUNT(MV.MinionId) AS MinionCount FROM MinionsVillains AS MV
+                            JOIN Villains AS V ON V.Id = MV.VillainId
+                            GROUP BY V.ID, V.Name
+                            HAVING COUNT(MV.MinionId) > 3
+                            ORDER BY V.Name";
+
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                SqlCommand command = new SqlCommand(query, dbCon);
+                var reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionCount"]}");
+                    }
+                }
+            }
+        }
+    }
+}
