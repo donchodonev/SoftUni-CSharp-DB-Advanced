56139ea4babbd6c69b5151fcea0d851de4dae3b5
donchodonev <dummyacc9292@gmail.com>
2021-08-28 11:23:33 +0300

    added first exercise solution added .docx exercise problem descriptions file

---
+++ 2.Entity Framework Introduction/Exercises/SoftUni/SoftUni/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //2.Database First exercise
+
+            //3.Employees Full Information
+        }
+    }
+}
