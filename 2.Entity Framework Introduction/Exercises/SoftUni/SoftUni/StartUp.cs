e81dfa3510842259b8cb7753d2de10423e0aeb87
donchodonev <dummyacc9292@gmail.com>
2021-08-28 12:36:55 +0300

    added two exercise solutions

--- 2.Entity Framework Introduction/Exercises/SoftUni/SoftUni/StartUp.cs
+++ 2.Entity Framework Introduction/Exercises/SoftUni/SoftUni/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using SoftUni.Data;
 using SoftUni.Models;
 
@@ -6,11 +8,77 @@ namespace SoftUni
 {
     public class StartUp
     {
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.LastName,
+                    x.MiddleName,
+                    x.JobTitle,
+                    x.Salary
+                }
+                )
+                .OrderBy(x => x.EmployeeId)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine(
+                    $"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context
+                .Employees.Select(x => new
+                {
+                    x.FirstName,
+                    x.Salary
+                })
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
         public static void Main(string[] args)
         {
             //2.Database First exercise
 
             //3.Employees Full Information
+
+            /*var softUniDbContext = new SoftUniContext();
+
+            var result = GetEmployeesFullInformation(softUniDbContext);
+
+            Console.WriteLine(result);*/
+
+            //4.Employees with Salary Over 50 000
+
+            var dbContext = new SoftUniContext();
+
+            var result = GetEmployeesWithSalaryOver50000(dbContext);
+
+            Console.WriteLine(result);
         }
     }
 }
