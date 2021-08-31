7eb316d7d5194d20395ca8f0fe027f276eaaab02
donchodonev <dummyacc9292@gmail.com>
2021-08-31 17:29:49 +0300

    added all models and db sets todo - further setup their relations

---
+++ 4.LINQ/MusicHub/Data/Models/Producer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MusicHub.Data.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        public string Pseudonym { get; set; }
+
+        public string Text { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Album> Albums { get; set; }
+    }
+}
---
+++ 4.LINQ/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public Album Album { get; set; }
+
+        public int WriterId { get; set; }
+
+        [Required]
+        public Writer Writer { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
