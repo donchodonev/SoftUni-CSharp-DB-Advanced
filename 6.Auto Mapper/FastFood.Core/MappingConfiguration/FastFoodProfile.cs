6ee37a0c670279d2980b3b012cb48792f2b04ac5
donchodonev <dummyacc9292@gmail.com>
2021-09-05 13:07:41 +0300

    setup OrdersController.cs added CreateOrderViewModel and CreateOrderInputModel mappings added constructor to CreateOrderViewModel and initialized both collection properties to avoid null reference exception

--- 6.Auto Mapper/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
+++ 6.Auto Mapper/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -1,7 +1,9 @@
 ﻿namespace FastFood.Core.MappingConfiguration
 {
     using AutoMapper;
+    using FastFood.Core.ViewModels.Orders;
     using FastFood.Models;
+    using System.Linq;
     using ViewModels.Positions;
 
     public class FastFoodProfile : Profile
@@ -14,6 +16,45 @@
 
             this.CreateMap<Position, PositionsAllViewModel>()
                 .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Orders
+                //Order input model
+            this.CreateMap<CreateOrderInputModel, Order>()
+                .ForMember(x => x.Customer, y => y.MapFrom(z => z.Customer))
+                .ForMember(x => x.EmployeeId, y => y.MapFrom(z => z.EmployeeId));
+
+            this.CreateMap<CreateOrderInputModel,Item>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.ItemId));
+
+            this.CreateMap<CreateOrderInputModel,OrderItem>()
+                .ForMember(x => x.Quantity, y => y.MapFrom(z => z.Quantity));
+
+            this.CreateMap<Order, CreateOrderInputModel>()
+                .ForMember(x => x.Customer, y => y.MapFrom(z => z.Customer))
+                .ForMember(x => x.EmployeeId, y => y.MapFrom(z => z.EmployeeId))
+                .ForMember(x => x.ItemId, y => y.MapFrom(z => z.OrderItems.Select(item => item.ItemId)))
+                .ForMember(x => x.Quantity, y => y.MapFrom(z => z.OrderItems.Select(qty => qty.Quantity)));
+
+                //Order view model
+            this.CreateMap<CreateOrderViewModel, Employee>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.Employees));
+
+            this.CreateMap<CreateOrderViewModel, Item>()
+                .ForMember(x => x.Id, y => y.MapFrom(z => z.Items));
+
+            this.CreateMap<Item, CreateOrderViewModel>()
+                .ForMember(x => x.Employees, y => y.MapFrom(z => z.Id));
+
+            this.CreateMap<Employee, CreateOrderViewModel>()
+                .ForMember(x => x.Items, y => y.MapFrom(z => z.Id));
+
+            //OrderAll view model
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.OrderId, y => y.MapFrom(z => z.Id))
+                .ForMember(x => x.Customer, y=> y.MapFrom(z => z.Customer))
+                .ForMember(x => x.Employee, y=> y.MapFrom(z => z.Employee.Name))
+                .ForMember(x => x.DateTime, y=> y.MapFrom(z => z.DateTime));
         }
     }
 }
--- 6.Auto Mapper/FastFood.Core/ViewModels/Orders/CreateOrderViewModel.cs
+++ 6.Auto Mapper/FastFood.Core/ViewModels/Orders/CreateOrderViewModel.cs
@@ -4,6 +4,11 @@
 
     public class CreateOrderViewModel
     {
+        public CreateOrderViewModel()
+        {
+            Items = new List<int>();
+            Employees = new List<int>();
+        }
         public List<int> Items { get; set; }
 
         public List<int> Employees { get; set; }
