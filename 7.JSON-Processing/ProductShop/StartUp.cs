4a9cb43fed83b61e4446053b4bbe2c21edc5f7e2
donchodonev <dummyacc9292@gmail.com>
2021-09-07 14:35:14 +0300

    added first exercise solution

--- 7.JSON-Processing/ProductShop/StartUp.cs
+++ 7.JSON-Processing/ProductShop/StartUp.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using AutoMapper;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using ProductShop.Data;
 using ProductShop.Models;
 
@@ -10,9 +12,24 @@ namespace ProductShop
 {
     public class StartUp
     {
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            User[] users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+
+            int countOfUsersAdded = context.SaveChanges();
+
+            return $"Successfully imported {countOfUsersAdded}";
+        }
+
         public static void Main(string[] args)
         {
+            var db = new ProductShopContext();
 
+            string serializedJsonData = File.ReadAllText(@".\..\..\..\Datasets\users.json");
+
+            Console.WriteLine(ImportUsers(db,serializedJsonData));
         }
     }
 }
