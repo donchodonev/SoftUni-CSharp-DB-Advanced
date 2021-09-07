689a83ba86bcfd1022d085669a87a717215eef50
donchodonev <dummyacc9292@gmail.com>
2021-09-07 20:58:38 +0300

    added CarDealer project ensured database is created

---
+++ 7.JSON-Processing/CarDealer/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var db = new CarDealerContext();
+
+            db.Database.EnsureCreated();
+        }
+    }
+}
