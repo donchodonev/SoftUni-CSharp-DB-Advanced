eafdf528c6463a0d6db251f24842c2b77e56511d
donchodonev <dummyacc9292@gmail.com>
2021-09-07 21:29:33 +0300

    Created new SupplierInput.cs DTO model add mapping between SupplierInput dto and Suppliers model added InitializeAutomapper() static method for easier mapping added first exercise solution

---
+++ 7.JSON-Processing/CarDealer/DTO/SupplierInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class SupplierInput
+    {
+        public string Name { get; set; }
+        public bool IsImporter { get; set; }
+    }
+}
--- 7.JSON-Processing/CarDealer/StartUp.cs
+++ 7.JSON-Processing/CarDealer/StartUp.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using AutoMapper;
 using CarDealer.Data;
+using CarDealer.DTO;
 using CarDealer.Models;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
@@ -12,11 +13,35 @@ namespace CarDealer
 {
     public class StartUp
     {
+        static IMapper mapper;
+        public static void InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(x => x.AddProfile<CarDealerProfile>());
+            mapper = new Mapper(config);
+        }
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            InitializeAutomapper();
+
+            var jsonData = JsonConvert.DeserializeObject<SupplierInput[]>(inputJson);
+
+            Supplier[] suppliers = mapper.Map<Supplier[]>(jsonData);
+
+            context.Suppliers.AddRange(suppliers);
+
+            int countOfImportedSuppliers = context.SaveChanges();
+
+            return $"Successfully imported {countOfImportedSuppliers}.";
+        }
         public static void Main(string[] args)
         {
             var db = new CarDealerContext();
 
-            db.Database.EnsureCreated();
+            //1. Import Suppliers
+
+            string supplierJsonData = File.ReadAllText(@".\..\..\..\Datasets\suppliers.json");
+
+            Console.WriteLine(ImportSuppliers(db, supplierJsonData));
         }
     }
 }
