b4ce1c605ce5c882d3b425500ac84679eb1ff736
donchodonev <dummyacc9292@gmail.com>
2021-09-09 13:13:11 +0300

    added first exercise solution

---
+++ 8.XML-Processing/ProductShop/Dtos/UsersInputDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos
+{
+    [XmlRoot]
+    [XmlType("User")]
+    public class UsersInputDTO
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public string Age { get; set; }
+    }
+}
---
+++ 8.XML-Processing/ProductShop/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using AutoMapper;
+using ProductShop.Data;
+using System.Xml.Linq;
+using ProductShop.Dtos;
+using System.Xml.Serialization;
+using System.IO;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        static IMapper mapper;
+
+        static void Initialize()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ProductShopProfile()));
+            mapper = new Mapper(config);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var serializer = new XmlSerializer(typeof(UsersInputDTO[]), new XmlRootAttribute("Users"));
+
+            var textRead = new StringReader(inputXml);
+
+            UsersInputDTO[] usersDTOs = (UsersInputDTO[])serializer.Deserialize(textRead);
+
+            User[] users = mapper.Map<User[]>(usersDTOs);
+
+            context.Users.AddRange(users);
+
+            int countOfUsersImported = context.SaveChanges();
+
+            return $"Successfully imported {countOfUsersImported}"; 
+        }
+        public static void Main(string[] args)
+        {
+            Initialize();
+
+            var db = new ProductShopContext();
+
+            /*          db.Database.EnsureDeleted();
+                        db.Database.EnsureCreated();
+            */
+
+
+            //1.Import Users
+
+            var xmlData = File.ReadAllText(@".\..\..\..\Datasets\users.xml");
+
+            Console.WriteLine(ImportUsers(db,xmlData));
+        }
+    }
+}
